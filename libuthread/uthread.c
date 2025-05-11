a44fbc115cabdc14b360dfb35c9fd7341b485f8c
Zachary Willson <43658160+zacharycwillson@users.noreply.github.com>
2025-05-10 19:35:37 -0700

    Project Skeleton files uploaded.
    Contains all files provided in projects/2.2 on CSIF

---
+++ libuthread/uthread.c
@@ -0,0 +1,51 @@
+#include <assert.h>
+#include <signal.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include "private.h"
+#include "uthread.h"
+#include "queue.h"
+
+struct uthread_tcb {
+	/* TODO Phase 2 */
+};
+
+struct uthread_tcb *uthread_current(void)
+{
+	/* TODO Phase 2/3 */
+}
+
+void uthread_yield(void)
+{
+	/* TODO Phase 2 */
+}
+
+void uthread_exit(void)
+{
+	/* TODO Phase 2 */
+}
+
+int uthread_create(uthread_func_t func, void *arg)
+{
+	/* TODO Phase 2 */
+}
+
+int uthread_run(bool preempt, uthread_func_t func, void *arg)
+{
+	/* TODO Phase 2 */
+}
+
+void uthread_block(void)
+{
+	/* TODO Phase 3 */
+}
+
+void uthread_unblock(struct uthread_tcb *uthread)
+{
+	/* TODO Phase 3 */
+}
+
