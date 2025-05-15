5b23a7f1afa2fdd7c0a892dc4f65a0fbb99eac01
Abdullah-alrawi2002 <105957901+Abdullah-alrawi2002@users.noreply.github.com>
2025-05-15 16:23:56 -0700

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -1,37 +1,84 @@
+#define _POSIX_C_SOURCE 200809L  // for sigaction, setitimer
+
 #include <signal.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-
+#include <unistd.h>
 #include "private.h"
 #include "uthread.h"
 
-/*
- * Frequency of preemption
- * 100Hz is 100 times per second
- */
 #define HZ 100
+
+static struct sigaction old_action;
+static struct itimerval   old_timer;
+static bool                use_preempt = false;
+
+static void preempt_handler(int signum)
+{
+    (void)signum;
+    preempt_disable();
+    uthread_yield();
+    preempt_enable();
+}
 
 void preempt_disable(void)
 {
-	/* TODO Phase 4 */
+    if (!use_preempt) return;
+
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGVTALRM);
+    sigprocmask(SIG_BLOCK, &mask, NULL);
 }
 
 void preempt_enable(void)
 {
-	/* TODO Phase 4 */
+    if (!use_preempt) return;
+
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGVTALRM);
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }
 
 void preempt_start(bool preempt)
 {
-	(void)preempt;
+    use_preempt = preempt;
+    if (!preempt) {
+        return;
+    }
+
+    struct sigaction sa;
+    sa.sa_handler = preempt_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGVTALRM, &sa, &old_action) < 0) {
+        perror("sigaction");
+        exit(1);
+    }
+    struct itimerval timer;
+    timer.it_interval.tv_sec  = 0;
+    timer.it_interval.tv_usec = 1000000 / HZ;
+    timer.it_value = timer.it_interval; 
+    if (setitimer(ITIMER_VIRTUAL, &timer, &old_timer) < 0) {
+        perror("setitimer");
+        exit(1);
+    }
 }
 
 void preempt_stop(void)
 {
-	/* TODO Phase 4 */
+    if (!use_preempt) return;
+    if (setitimer(ITIMER_VIRTUAL, &old_timer, NULL) < 0) {
+        perror("setitimer restore");
+        exit(1);
+    }
+    if (sigaction(SIGVTALRM, &old_action, NULL) < 0) {
+        perror("sigaction restore");
+        exit(1);
+    }
 }
 
