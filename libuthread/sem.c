e63f17f34b46888a5797175c190deee11cdd4bfe
Abdullah-alrawi2002 <105957901+Abdullah-alrawi2002@users.noreply.github.com>
2025-05-15 16:04:26 -0700

    Update sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -1,71 +1,64 @@
 #include <stddef.h>
 #include <stdlib.h>
-
 #include "queue.h"
 #include "private.h"
 #include "sem.h"
 
 struct semaphore {
-	int     value;
-	queue_t waiting; 
+    int     value;
+    queue_t waiting;
 };
 
 sem_t sem_create(size_t count)
 {
-	sem_t s = malloc(sizeof(*s));
-	if (!s) return NULL;
+    sem_t s = malloc(sizeof(*s));
+    if (!s) return NULL;
 
-	s->value   = (int)count;
-	s->waiting = queue_create();
-	if (!s->waiting) {
-		free(s);
-		return NULL;
-	}
-	return s;
+    s->value   = (int)count;
+    s->waiting = queue_create();
+    if (!s->waiting) {
+        free(s);
+        return NULL;
+    }
+    return s;
 }
 
 int sem_destroy(sem_t sem)
 {
-	if (!sem) return -1;
-	if (queue_length(sem->waiting) > 0)
-		return -1;
-
-	queue_destroy(sem->waiting);
-	free(sem);
-	return 0;
+    if (!sem) return -1;
+    if (queue_length(sem->waiting) > 0)
+        return -1;
+    queue_destroy(sem->waiting);
+    free(sem);
+    return 0;
 }
 
 int sem_down(sem_t sem)
 {
-	if (!sem) return -1;
+    if (!sem) return -1;
+    preempt_disable();
+    sem->value--;
+    if (sem->value < 0) {
+        struct uthread_tcb *me = uthread_current();
+        queue_enqueue(sem->waiting, me);
+        uthread_block();
+    }
 
-	preempt_disable();
-
-	if (sem->value > 0) {
-		sem->value--;
-	} else {
-		struct uthread_tcb *me = uthread_current();
-		queue_enqueue(sem->waiting, me);
-		uthread_block();
-	}
-
-	preempt_enable();
-	return 0;
+    preempt_enable();
+    return 0;
 }
 
 int sem_up(sem_t sem)
 {
-	if (!sem) return -1;
+    if (!sem) return -1;
+    preempt_disable();
+    sem->value++;
+    if (sem->value <= 0) {
+        void *td;
+        queue_dequeue(sem->waiting, &td);
+        uthread_unblock((struct uthread_tcb*)td);
+    }
 
-	preempt_disable();
-	if (queue_length(sem->waiting) > 0) {
-		void *td;
-		queue_dequeue(sem->waiting, &td);
-		uthread_unblock((struct uthread_tcb*)td);
-	} else {
-		sem->value++;
-	}
-
-	preempt_enable();
-	return 0;
+    preempt_enable();
+    return 0;
 }
